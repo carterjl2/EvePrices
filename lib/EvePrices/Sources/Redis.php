3ab673dbcc97616586081b097df25e1500a10fbb
Steve Anderson <steve@fuzzwork.co.uk>
2014-01-24 18:03:08 +0100

    Updated with two new methods

--- lib/EvePrices/Sources/Redis.php
+++ lib/EvePrices/Sources/Redis.php
@@ -27,4 +27,49 @@ class Redis
         }
         return array($price,$pricebuy);
     }
+
+
+    public function returnPriceArray($typeids, $regionid)
+    {
+        priceArray=array();
+        foreach ($typeids as $typeid) {
+            $pricedatasell=$this->redis->get($regionid.'sell-'.$typeid);
+            $pricedatabuy=$this->redis->get($regionid.'buy-'.$typeid);
+            $values=explode("|", $pricedatasell);
+            $price=$values[0];
+            if (!(is_numeric($price))) {
+                $price=0;
+            }
+            $values=explode("|", $pricedatabuy);
+            $pricebuy=$values[0];
+            if (!(is_numeric($pricebuy))) {
+                $pricebuy=0;
+            }
+            $priceArray[$typeid]=array($price,$pricebuy);
+        }
+        return $priceArray;
+    }
+    
+    public function populateArray($inputarray, $regionid)
+    {
+        populatedArray=array();
+        foreach ($inputarray as $entry) {
+            $typeid=$entry['typeid'];
+            $pricedatasell=$this->redis->get($regionid.'sell-'.$typeid);
+            $pricedatabuy=$this->redis->get($regionid.'buy-'.$typeid);
+            $values=explode("|", $pricedatasell);
+            $price=$values[0];
+            if (!(is_numeric($price))) {
+                $price=0;
+            }
+            $values=explode("|", $pricedatabuy);
+            $pricebuy=$values[0];
+            if (!(is_numeric($pricebuy))) {
+                $pricebuy=0;
+            }
+            $entry['price']=array($price,$pricebuy);
+            $populatedArray[]=$entry;
+        }
+        return $populatedArray;
+    }
 }
