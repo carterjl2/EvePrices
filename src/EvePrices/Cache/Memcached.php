c1fafa21049ea2c7043c7397da786eafbec65357
Steve Anderson <steve@fuzzwork.co.uk>
2014-01-26 01:53:12 +0100

    updated the layout, added caching module

---
+++ src/EvePrices/Cache/Memcached.php
@@ -0,0 +1,92 @@
+<?php
+namespace EvePrices\Cache;
+
+class Memcached
+{
+
+    private $memcache;
+
+    public function __construct($host, $port)
+    {
+        $this->memcache = new \Memcache;
+        $this->memcache->connect($host, $port);
+    }
+
+    public function checkPrice($typeid, $regionid)
+    {
+        $pricedatasell=$this->memcache->get('cache'.$regionid.'sell-'.$typeid);
+        $pricedatabuy=$this->memcache->get('cache'.$regionid.'buy-'.$typeid);
+        if (!(is_numeric($pricedatasell))) {
+            return null;
+        }
+        if (!(is_numeric($pricedatabuy))) {
+            return null;
+        }
+        return array($pricedatasell,$pricedatabuy);
+    }
+
+    public function checkPriceArray($typeids, $regionid)
+    {
+        $priceArray=array();
+        $toget=array();
+        foreach ($typeids as $typeid) {
+            $pricedatasell=$this->memcache->get('cache'.$regionid.'sell-'.$typeid);
+            $pricedatabuy=$this->memcache->get('cache'.$regionid.'buy-'.$typeid);
+            if (!(is_numeric($pricedatasell))) {
+                $toget[]=$typeid;
+                continue;
+            }
+            if (!(is_numeric(pricedatabuy))) {
+                $toget[]=$typeid;
+                continue;
+            }
+            $priceArray[$typeid]=array($pricedatasell,$pricedatabuy);
+        }
+        return array($priceArray,$toget);
+    }
+
+    public function checkPopulatedArray($inputarray, $regionid)
+    {
+        $populatedArray=array();
+        $toget=array();
+        foreach ($inputarray as $entry) {
+            $typeid=$entry['typeid'];
+            $pricedatasell=$this->memcache->get('cache'.$regionid.'sell-'.$typeid);
+            $pricedatabuy=$this->memcache->get('cache'.$regionid.'buy-'.$typeid);
+            if (!(is_numeric($pricedatasell))) {
+                $toget[]=$entry;
+                continue;
+            }
+            if (!(is_numeric($pricedatabuy))) {
+                $toget[]=$entry;
+                continue;
+            }
+                $entry['price']=array($pricedatasell,$pricedatabuy);
+                $populatedArray[]=$entry;
+        }
+        return array($populatedArray,$toget);
+    }
+
+
+    public function setPrice($typeid, $regionid, $pricearray)
+    {
+        $this->memcache->set('cache'.$regionid.'sell-'.$typeid, $pricearray[0], 3600);
+        $this->memcache->set('cache'.$regionid.'buy-'.$typeid, $pricearray[1], 3600);
+    }
+
+    public function setPriceArray($typeids, $regionid)
+    {
+        foreach ($typeids as $typeid => $pricearray) {
+            $this->memcache->set('cache'.$regionid.'sell-'.$typeid, $pricearray[0], 3600);
+            $this->memcache->set('cache'.$regionid.'buy-'.$typeid, $pricearray[1], 3600);
+        }
+    }
+
+    public function setPopulatedArray($inputarray, $regionid)
+    {
+        foreach ($inputarray as $entry) {
+            $this->memcache->set('cache'.$regionid.'sell-'.$entry['typeid'], $entry['price'][0], 3600);
+            $this->memcache->set('cache'.$regionid.'buy-'.$entry['typeid'], $entry['price'][1], 3600);
+        }
+    }
+}
---
+++ src/EvePrices/EvePrices.php
@@ -0,0 +1,101 @@
+<?php
+namespace EvePrices;
+
+class EvePrices
+{
+
+    const VERSION="1.0.0";
+
+    private $region;
+    private $priceSource;
+    private $cache;
+
+    public function __construct($args = array('source'=>'redis','region'=>10000002,'host'=>'127.0.0.1','port'=>6379,'scheme'=>'tcp'))
+    {
+        switch ($args['source']) {
+            case 'redis':
+                $this->priceSource=new \EvePrices\Sources\Redis($args['host'], $args['port'], $args['scheme']);
+                break;
+            case 'populatedmemcached':
+                $this->priceSource=new \EvePrices\Sources\Memcached($args['host'], $args['port']);
+                break;
+            case 'mysql':
+                $this->priceSource=new \EvePrices\Sources\Mysql($args['dbh'], $args['selltable'], $args['buytable']);
+                break;
+            case 'marketdata':
+                $this->priceSource=new \EvePrices\Sources\MarketData($args['userid']);
+                break;
+            default:
+                throw new \Exception("EvePrices doesn't understand source type ".$args['source']);
+        }
+        switch ($args['cache']) {
+            case 'memcache':
+                $this->cache=new \EvePrices\Cache\Memcached($args['cachehost'], $args['cacheport']);
+        }
+        if (isset($args['region']) and is_numeric($args['region'])) {
+            $this->region=$args['region'];
+        } else {
+            $this->region=10000002;
+        }
+        if ($this->region==10000002) {
+            $this->region='forge';
+        }
+    }
+
+    public function setRegion($regionid)
+    {
+        if (isset($regionid) and is_numeric($regionid)) {
+            $this->region=$regionid;
+        } else {
+            throw new \Exception("regionid must be a number");
+        }
+        if ($this->region==10000002) {
+            $this->region='forge';
+        }
+    }
+
+    public function returnPrice($typeid, $regionid = null)
+    {
+        if (isset($this->cache)) {
+            $cacheprice=$this->cache->checkPrice($typeid, $regionid ?: $this->region);
+            if (isset($cacheprice)) {
+                return $cacheprice;
+            }
+        }
+        $prices=$this->priceSource->returnPrice($typeid, $regionid ?: $this->region);
+        if (isset($this->cache)) {
+            $this->cache->setPrice($typeid, $regionid ?: $this->region, $prices);
+        }
+        return $prices;
+    }
+
+    public function returnPriceArray($typeids, $regionid = null)
+    {
+        $cachedpricearray=array();
+        if (isset($this->cache)) {
+            list($cachedpricearray, $typeids)=$this->cache->checkPriceArray($typeids, $regionid ?: $this->region);
+        }
+        $pricearray=$this->priceSource->returnPriceArray($typeids, $typeids, $regionid ?: $this->region);
+        if (isset($this->cache)) {
+            $this->cache->setPriceArray($pricearray, $regionid ?: $this->region);
+            $totalpricearray=$pricearray+$cachedpricearray;
+            $pricearray=$totalpricearray;
+        }
+        return $pricearray;
+    }
+    
+    public function populateArray($inputarray, $regionid = null)
+    {
+        $cachedpricearray=array();
+        if (isset($this->cache)) {
+            list($cachedpricearray, $inputarray)=$this->cache->checkPopulatedArray($typeids, $regionid ?: $this->region);
+        }
+        $pricearray=$this->priceSource->populateArray($inputarray, $typeids, $regionid ?: $this->region);
+        if (isset($this->cache)) {
+            $this->cache->setPopulatedArray($pricearray, $regionid ?: $this->region);
+            $totalpricearray=$pricearray+$cachedpricearray;
+            $pricearray=$totalpricearray;
+        }
+        return $pricearray;
+    }
+}
