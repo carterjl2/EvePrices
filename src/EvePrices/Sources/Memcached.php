4f1802b6d8161b1d7952df714e196f972984e3ca
Steve Anderson <steve@fuzzwork.co.uk>
2014-01-26 02:05:34 +0100

    changed stubs to working code

--- src/EvePrices/Sources/Memcached.php
+++ src/EvePrices/Sources/Memcached.php
@@ -31,11 +31,45 @@ class Memcached
 
     public function returnPriceArray($typeids, $regionid)
     {
-
+        $priceArray=array();
+        foreach ($typeids as $typeid) {
+            $pricedatasell=$this->memcache->get($regionid.'sell-'.$typeid);
+            $pricedatabuy=$this->memcache->get($regionid.'buy-'.$typeid);
+            $values=explode("|", $pricedatasell);
+            $price=$values[0];
+            if (!(is_numeric($price))) {
+                $price=0;
+            }
+            $values=explode("|", $pricedatabuy);
+            $pricebuy=$values[0];
+            if (!(is_numeric($pricebuy))) {
+                $pricebuy=0;
+            }
+            $priceArray[$typeid]=array($price,$pricebuy);
+        }
+        return $priceArray;
     }
 
     public function populateArray($inputarray, $regionid)
     {
-
+        $populatedArray=array();
+        foreach ($inputarray as $entry) {
+            $typeid=$entry['typeid'];
+            $pricedatasell=$this->memcache->get($regionid.'sell-'.$typeid);
+            $pricedatabuy=$this->memcache->get($regionid.'buy-'.$typeid);
+            $values=explode("|", $pricedatasell);
+            $price=$values[0];
+            if (!(is_numeric($price))) {
+                $price=0;
+            }
+            $values=explode("|", $pricedatabuy);
+            $pricebuy=$values[0];
+            if (!(is_numeric($pricebuy))) {
+                $pricebuy=0;
+            }
+            $entry['price']=array($price,$pricebuy);
+            $populatedArray[]=$entry;
+        }
+        return $populatedArray;
     }
 }
