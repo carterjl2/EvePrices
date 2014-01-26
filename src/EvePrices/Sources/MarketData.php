2c4672efd4d6809b77c1e4b43107665f66834017
Steve Anderson <steve@fuzzwork.co.uk>
2014-01-26 05:08:12 +0100

    updated with array handlers

--- src/EvePrices/Sources/MarketData.php
+++ src/EvePrices/Sources/MarketData.php
@@ -4,20 +4,15 @@ namespace EvePrices\Sources;
 class MarketData
 {
 
-    private $cache;
     private $userid;
 
     public function __construct($userid)
     {
-        $this->cache =array();
         $this->userid=$userid;
     }
 
     public function returnPrice($typeid, $regionid)
     {
-        if (isset($this->cache[$regionid-$typeid])) {
-            return $this->cache[$regionid-$typeid];
-        }
         $url="http://api.eve-marketdata.com/api/item_prices2.xml?char_name=".$this->userid
         ."&buysell=a&region_ids=".$regionid."&type_ids=".$typeid;
         $pricexml=file_get_contents($url);
@@ -34,15 +29,46 @@ class MarketData
         if (!(is_numeric($buyprice))) {
             $buyprice=0;
         }
-        $this->cache[$regionid-$typeid]=array($price,$buyprice);
         return array($price,$buyprice);
     }
 
     public function returnPriceArray($typeids, $regionid)
     {
+        $priceArray=array();
+        $url="http://api.eve-marketdata.com/api/item_prices2.xml?char_name=".$this->userid
+        ."&buysell=a&region_ids=".$regionid."&type_ids=".join(",", $typeid);
+        $pricexml=file_get_contents($url);
+        $xml=new \SimpleXMLElement($pricexml);
+        foreach ($typeids as $typeid) {
+            $price= $xml->xpath('//row[@buysell="s"][@typeID="'.$typeid.'"]/@price');
+            $price=(float)$price[0]->price;
+            $price=round($price, 2);
+            if (!(is_numeric($price))) {
+                $price=0;
+            }
+            $buyprice= $xml->xpath('//row[@buysell="b"][@typeID="'.$typeid.'"]/@price');
+            $buyprice=(float)$buyprice[0]->price;
+            $buyprice=round($buyprice, 2);
+            if (!(is_numeric($buyprice))) {
+                $buyprice=0;
+            }
+            $priceArray[$typeid]=array($price,$buyprice);
+        }
+        return $priceArray;
     }
 
     public function populateArray($inputarray, $regionid)
     {
+        $typeids=array();
+        $populatedArray=array();
+        foreach ($inputarray as $entry) {
+            $typeids[]=$entry['typeid'];
+        }
+        $pricearray=$this->returnPriceArray($typeids, $regionid);
+        foreach ($inputarray as $entry) {
+            $entry['price']=$pricearray[$entry['typeid']];
+            $populatedArray[]=$entry;
+        }
+        return $populatedArray;
     }
 }
